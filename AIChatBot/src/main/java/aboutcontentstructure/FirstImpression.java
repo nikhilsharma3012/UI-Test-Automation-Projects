2a58e8f817a359b80ceaf865c5a75cfb652f0a5b
nikhilsharma3012 <65644058+nikhilsharma3012@users.noreply.github.com>
2022-08-18 17:55:28 +0530

    Update FirstImpression.java

--- AIChatBot/src/main/java/aboutcontentstructure/FirstImpression.java
+++ AIChatBot/src/main/java/aboutcontentstructure/FirstImpression.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import pagebase.BasePage;
 
 /**
- * created by Jyoti 07/10/2019
+ * created by Nikhil Sharma 07/10/2019
  */
 
 public class FirstImpression extends BasePage
